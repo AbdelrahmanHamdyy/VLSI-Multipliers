9093718c53768d87d6652232138413caa5ee2d06
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-12-16 21:16:15 +0200

    Add enables

--- src/rtl/multiplierTree/multiplierTree.v
+++ src/rtl/multiplierTree/multiplierTree.v
@@ -10,11 +10,11 @@ wire [31:0] a_out;
 wire [31:0] b_out;
 wire [63:0] c_out;
 
-buffer #(32) inRegA (clk, rst, a, a_out);
-buffer #(32) inRegB (clk, rst, b, b_out);
+buffer #(32) inRegA (clk, rst, en, a, a_out);
+buffer #(32) inRegB (clk, rst, en, b, b_out);
 
 wallace M64 (a_out, b_out, c_out);
 
-buffer #(64) outReg (clk, rst, c_out, c);
+buffer #(64) outReg (clk, rst, en, c_out, c);
 
 endmodule
--- src/rtl/simpleMultiplier/simpleMultiplier.v
+++ src/rtl/simpleMultiplier/simpleMultiplier.v
@@ -1,6 +1,7 @@
 module simpleMultiplier (
     input clk,
     input rst,
+    input en,
     input [31:0] a,
     input [31:0] b,
     output [63:0] c
@@ -10,11 +11,11 @@ wire [31:0] a_out;
 wire [31:0] b_out;
 wire [63:0] c_out;
 
-buffer #(32) inRegA (clk, rst, a, a_out);
-buffer #(32) inRegB (clk, rst, b, b_out);
+buffer #(32) inRegA (clk, rst, en, a, a_out);
+buffer #(32) inRegB (clk, rst, en, b, b_out);
 
 multOperator M64 (clk, rst, a_out, b_out, c_out);
 
-buffer #(64) outReg (clk, rst, c_out, c);
+buffer #(64) outReg (clk, rst, en, c_out, c);
     
 endmodule
