228a13799576d302bf87bcf5bf416d5d319df054
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-01-05 19:11:23 +0200

    Post-submission

--- src/rtl/simpleMultiplier/multOperator.v
+++ src/rtl/simpleMultiplier/multOperator.v
@@ -1,9 +1,9 @@
 module multOperator (
     input clk,
     input rst,
-    input [31:0] a,
-    input [31:0] b,
-    output reg [63:0] c
+    input signed [31:0] a,
+    input signed [31:0] b,
+    output reg signed [63:0] c
 );
 
 always @(posedge clk, posedge rst) begin
